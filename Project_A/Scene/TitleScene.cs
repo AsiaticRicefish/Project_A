2cc038b73341bdadff5786fdca124ab3a2fd3e26
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-04-08 10:32:10 +0900

    [fix] 타이틀이 출력이 안되는 현상 고침
    게임이 동작할 때 Start 함수를 설정을 안 한 것이 원인으로 발생함
    
    Start() 추가

---
+++ Project_A/Scene/TitleScene.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A.Scene
+{
+    public class TitleScene : BaseScene
+    {
+        public override void Render()
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine("*        제목을 입력하시오         *");
+            Console.WriteLine("************************************");
+            Console.WriteLine("");
+            Console.WriteLine("계속하려면 아무키나 누르십시오.");
+
+        }
+
+        public override void Input()
+        {
+           Console.ReadKey(true); // 무슨 키를 눌렀는지 안보이도록 함
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public override void Result()
+        {
+            
+        }
+
+     
+    }
+}
