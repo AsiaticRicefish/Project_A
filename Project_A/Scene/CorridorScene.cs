bcbf2c030b08b2439fa472dde012498df62fddee
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-04-08 13:12:16 +0900

    [feat] 복도맵 구현 테스트
    복도맵 간단히 구현 (추후 수정 예정)

--- Project_A/Scene/CorridorScene.cs
+++ Project_A/Scene/CorridorScene.cs
@@ -9,10 +9,42 @@ namespace Project_A.Scene
     public class CorridorScene : BaseScene
     {
         private ConsoleKey input;
+        private string[] mapData;
+        private bool[,] map;
+
+        public CorridorScene()
+        {
+            mapData = new string[]
+            {
+            "■■■■■■■",
+            "■     ■",
+            "■     ■",
+            "      ■",
+            "■      ",
+            "■■■■■■■", 
+            };
+
+            map = new bool[6, 7];
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    map[y, x] = mapData[y][x] == '■' ? false : true;
+                }
+                Console.WriteLine();
+            }
+
+        }
 
         public override void Render()
         {
-            Console.WriteLine("복도로 나서자, 조명이 깜빡거립니다...");
+            for (int i = 0; i < 15; i++) // 깜빡거리는 연출 넣기
+            {
+                Util.Print("복도로 나서자, 조명이 깜빡거린다...", ConsoleColor.White, 100);
+                Console.Clear();
+            }
+
+            PrintMap();
         }
         public override void Input()
         {
@@ -26,5 +58,26 @@ namespace Project_A.Scene
         {
 
         }     
+
+        private void PrintMap()
+        {
+            Console.SetCursorPosition(0, 0); // 처음으로 위치시킴
+
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] == true)
+                    {
+                        Console.Write(' ');
+                    }
+                    else 
+                    {
+                        Console.Write('■');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
