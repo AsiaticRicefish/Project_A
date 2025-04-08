887e09aad1f67fa36063fdd6785bd347672e261f
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-04-08 10:07:48 +0900

    [feat] Basic Game, Scene
    게임 작동을 위한 Game 클래스 구성과 기본적인 신의 토대 구성

---
+++ Project_A/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_A.Scene;
+
+namespace Project_A
+{
+    public static class Game
+    {
+
+        private static Dictionary<string, BaseScene> sceneDic;
+        private static BaseScene curScene;
+
+        private static bool gameOver;
+
+        public static void Run()
+        {
+            while (gameOver == false)
+            {
+                curScene.Render();
+                curScene.Input();
+                curScene.Update();
+                curScene.Result();
+            }
+
+        }
+
+
+        private static void Start()
+        {
+            gameOver = false;
+
+            sceneDic = new Dictionary<string, BaseScene>();
+            sceneDic.Add("Title", new TitleScene());
+
+            curScene = sceneDic["Title"];
+
+        }
+
+        public static void End() 
+        {
+
+        }
+
+    }
+}
